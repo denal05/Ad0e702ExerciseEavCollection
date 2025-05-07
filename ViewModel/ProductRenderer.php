993f22c63270880f6af73d4f384069245665bc4f
Denis Aleksandrov <denis.v.aleksandrov@gmail.com>
2025-05-08 00:12:19 +0200

    Commented-out the line in ViewModel/ProductRenderer.php that caused the TypeError: Cannot access offset of type string on string... Now the URL https://ad0-e702.local/collection/product/ correctly returns 1891 rows as in the equivalent SQL query documented on line 47 of ViewModel/ProductRenderer.php

--- ViewModel/ProductRenderer.php
+++ ViewModel/ProductRenderer.php
@@ -33,8 +33,19 @@ class ProductRenderer implements ArgumentInterface
     {
         try {
             $productCollection = $this->_productCollectionFactory->create();
-            $productCollection->addAttributeToFilter(['name', 'url_key']);
+
+            /* The line below generates the following error:
+               TypeError: Cannot access offset of type string on string in /var/www/ad0-e702/vendor/magento/module-eav/Model/Entity/Collection/AbstractCollection.php:395
+               Stack trace:
+               #0 /var/www/ad0-e702/vendor/magento/module-catalog/Model/ResourceModel/Product/Collection.php(1630): Magento\Eav\Model\Entity\Collection\AbstractCollection->addAttributeToFilter()
+               #1 /var/www/ad0-e702/app/code/Denal05/Ad0e702ExerciseEavCollection/ViewModel/ProductRenderer.php(36): Magento\Catalog\Model\ResourceModel\Product\Collection->addAttributeToFilter()
+               #2 /var/www/ad0-e702/app/code/Denal05/Ad0e702ExerciseEavCollection/view/frontend/templates/index.phtml(6): Denal05\Ad0e702ExerciseEavCollection\ViewModel\ProductRenderer->getProductCollection()
+            */
+            ////$productCollection->addAttributeToFilter(['name', 'url_key']);
+
             $productCollection->addFieldToFilter('type_id', \Magento\Catalog\Model\Product\Type::TYPE_SIMPLE);
+            // SQL: SELECT 'e'.* FROM 'catalog_product_entity' AS 'e' WHERE ('e'.'type_id' = 'simple')
+
             return $productCollection;
         } catch (\Exception $exception) {
             return null;
